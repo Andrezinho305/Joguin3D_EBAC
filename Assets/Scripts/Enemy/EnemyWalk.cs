3e2fde97aad694889a0e08b2410e850f7c8f79c4
André Tavares <andre.t.teixeira@hotmail.com>
2024-05-30 11:19:40 -0300

    Enemy Settup
    Added waypoint movements to enemies

---
+++ Assets/Scripts/Enemy/EnemyWalk.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyWalk : EnemyBase
+    {
+
+
+        [Header("Movement")]
+        public GameObject[] waypoints;
+        public float minDistance = 1f;
+        public float speed = 10f;
+
+
+        private int _index = 0;
+
+
+        private void Update()
+        {
+            if(Vector3.Distance(transform.position, waypoints[_index].transform.position) < minDistance) //verifica se a posição atual em relação ao waypoint é menor que o definido
+            {
+                _index++;
+                if(_index >= waypoints.Length)
+                {
+                    _index = 0;
+                }
+            }
+
+                transform.position = Vector3.MoveTowards(transform.position, waypoints[_index].transform.position, Time.deltaTime * speed);
+
+        }
+
+
+
+
+
+    }
+}
