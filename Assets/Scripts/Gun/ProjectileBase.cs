23cbc225a8ece45b2b94edaf648203b97a819937
André Tavares <andre.t.teixeira@hotmail.com>
2024-06-15 11:05:08 -0300

    Fixed Spread Gun Bug

--- Assets/Scripts/Gun/ProjectileBase.cs
+++ Assets/Scripts/Gun/ProjectileBase.cs
@@ -23,32 +23,21 @@ public class ProjectileBase : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        foreach (var t in tagsToHit)
+
+       var projectile = collision.collider.GetComponent<ProjectileBase>(); //verifica se houve colisao com o projetil
+
+       if (projectile != null) return; //se o projetil colidiu com si mesmo, nao faz a proxima parte do codigo
+
+
+       var damageable = collision.transform.GetComponent<IDamageable>();
+
+        if (damageable != null)
         {
-            if (collision.transform.tag == t)
-            {
+            Vector3 dir = collision.transform.position - transform.position; //identifica a direção da colisao e calcula para onde mover o inimigo
+            dir = -dir.normalized; //retorna o vetor para 1 independente da conta anterior
+            dir.y = 0;
 
-                var projectile = collision.collider.GetComponent<ProjectileBase>(); //verifica se houve colisao com o projetil
-
-                if (projectile != null) return; //se o projetil colidiu com si mesmo, nao faz a proxima parte do codigo
-
-
-                var damageable = collision.transform.GetComponent<IDamageable>();
-
-                if (damageable != null)
-                {
-                    Vector3 dir = collision.transform.position - transform.position; //identifica a direção da colisao e calcula para onde mover o inimigo
-                    dir = -dir.normalized; //retorna o vetor para 1 independente da conta anterior
-                    dir.y = 0;
-
-                    damageable.Damage(damageAmount, dir);
-
-                }
-
-                break;
-
-            }
-
+            damageable.Damage(damageAmount, dir);
         }
 
         Destroy(gameObject);
